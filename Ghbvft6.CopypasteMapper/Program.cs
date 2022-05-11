afa36e13f2d6cf6bc3a58e1c76ec7bcec60f70c6
greg-chuchro <gchuchro@acm.org>
2022-05-11 13:49:20 +0900

    init

---
+++ Ghbvft6.CopypasteMapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ghbvft6.CopypasteMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
