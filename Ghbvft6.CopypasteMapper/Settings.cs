63860d78c1d419a05079249e2ba67d6341920bcc
greg-chuchro <gchuchro@acm.org>
2022-05-15 22:15:26 +0900

    #1 regex mapper (#3)

---
+++ Ghbvft6.CopypasteMapper/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghbvft6.CopypasteMapper {
+    public class Settings {
+        public class Mapping {
+            public string MatchPattern;
+            public string ReplacementPattern;
+            public string Replacement;
+        }
+
+        public int delay;
+        public Mapping[] mappings;
+    }
+}
