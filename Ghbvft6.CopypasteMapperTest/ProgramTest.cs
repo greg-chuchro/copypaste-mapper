6eb7c1142e29ea5e9946f7f57646c9e89c94b566
greg-chuchro <gchuchro@acm.org>
2022-05-18 13:27:29 +0900

    #7 json settings (#8)

--- Ghbvft6.CopypasteMapperTest/ProgramTest.cs
+++ Ghbvft6.CopypasteMapperTest/ProgramTest.cs
@@ -1,8 +1,9 @@
 using Ghbvft6.CopypasteMapper;
 using System;
+using System.IO;
 using System.Runtime.ExceptionServices;
+using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using Xunit;
 
@@ -30,10 +31,31 @@ namespace Ghbvft6.CopypasteMapperTest {
             var settings = new Settings();
             settings.mappings = new[] {
                 new Settings.Mapping {
-                    MatchPattern = @"C:\\.*",
-                    ReplacementPattern = @"\\",
-                    Replacement = @"/",
+                    matchPattern = @"C:\\.*",
+                    replacementPattern = @"\\",
+                    replacement = @"/",
                 }
+            };
+
+            ExecuteInSTAState(() => {
+                Clipboard.SetText("C:\\test");
+                Program.Remap(settings);
+                Assert.Equal("C:/test", Clipboard.GetText());
+            });
+        }
+
+        [Fact]
+        public void Test2() {
+            File.Copy("Ghbvft6.CopypasteMapper.Settings.json", $"{Program.ConfigDir.FullName}/Ghbvft6.CopypasteMapper.Settings.json", true);
+
+            var path = Program.GetJsonPath<Settings>();
+            var json = File.ReadAllText(path);
+            var newSettings = JsonSerializer.Deserialize<Settings>(json, new JsonSerializerOptions {
+                IncludeFields = true
+            }) ?? throw new Exception($"Couldn't read {path}");
+            var settings = new Settings() {
+                delay = newSettings.delay,
+                mappings = newSettings.mappings
             };
 
             ExecuteInSTAState(() => {
