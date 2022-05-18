5f548ab25bcda48c1fbafa52bc1549f80e94b6e3
greg-chuchro <gchuchro@acm.org>
2022-05-18 13:27:29 +0900

    #7 json settings (#8)

--- Ghbvft6.CopypasteMapper/Program.cs
+++ Ghbvft6.CopypasteMapper/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
@@ -22,6 +25,18 @@ namespace Ghbvft6.CopypasteMapper {
         const int CLIPBRD_E_CANT_OPEN = unchecked((int)0x800401D0);
         const uint CF_UNICODETEXT = 13;
 
+        public static DirectoryInfo ConfigDir { get; }
+        private static Settings? settings;
+
+        static Program() {
+            var configPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/{Process.GetCurrentProcess().ProcessName}";
+            ConfigDir = Directory.CreateDirectory(configPath);
+        }
+
+        public static string GetJsonPath<T>() {
+            return $"{ConfigDir.FullName}/{typeof(T).FullName}.json";
+        }
+
         public static void SetText(string text) {
             OpenClipboard(default);
             try {
@@ -39,36 +54,45 @@ namespace Ghbvft6.CopypasteMapper {
             if (Clipboard.ContainsText()) {
                 var clipboardText = Clipboard.GetText();
                 foreach (var mapping in settings.mappings) {
-                    var match = Regex.Match(clipboardText, mapping.MatchPattern);
+                    var match = Regex.Match(clipboardText, mapping.matchPattern);
                     if (match.Length == clipboardText.Length) {
-                        var newText = Regex.Replace(clipboardText, mapping.ReplacementPattern, mapping.Replacement);
+                        var newText = Regex.Replace(clipboardText, mapping.replacementPattern, mapping.replacement);
                         SetText(newText);
                     }
                 }
             }
         }
 
-        private static void RemapLoop(Settings settings) {
+        private static void RemapLoop() {
             while (true) {
                 try {
-                    Remap(settings);
+                    Remap(settings!);
                 } catch (COMException ex) when (ex.HResult == CLIPBRD_E_CANT_OPEN) { }
-                Thread.Sleep(settings.delay);
+                Thread.Sleep(settings!.delay);
             }
         }
 
         [STAThread]
         static void Main(string[] args) {
-            var settings = new Settings();
-            settings.delay = 100;
-            settings.mappings = new[] {
-                new Settings.Mapping {
-                    MatchPattern = @"C:\\.*",
-                    ReplacementPattern = @"\\",
-                    Replacement = @"/",
+            new Thread(() => {
+                while (true) {
+                    var path = GetJsonPath<Settings>();
+                    var json = File.ReadAllText(path);
+                    var newSettings = JsonSerializer.Deserialize<Settings>(json, new JsonSerializerOptions {
+                        IncludeFields = true
+                    }) ?? throw new Exception($"Couldn't read {path}");
+                    settings = new() {
+                        delay = newSettings.delay,
+                        mappings = newSettings.mappings
+                    };
+
+                    Thread.Sleep(1000);
                 }
-            };
-            RemapLoop(settings);
+            }).Start();
+            while (settings == null) {
+                Thread.Sleep(100);
+            }
+            RemapLoop();
         }
     }
 }
--- Ghbvft6.CopypasteMapper/Settings.cs
+++ Ghbvft6.CopypasteMapper/Settings.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace Ghbvft6.CopypasteMapper {
     public class Settings {
         public class Mapping {
-            public string MatchPattern;
-            public string ReplacementPattern;
-            public string Replacement;
+            public string matchPattern = "";
+            public string replacementPattern = "";
+            public string replacement = "";
         }
 
-        public int delay;
-        public Mapping[] mappings;
+        public int delay = 100;
+        public Mapping[] mappings = Array.Empty<Mapping>();
     }
 }
